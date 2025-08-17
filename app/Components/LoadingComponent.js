7532b65b8b137e86be89358e636ea5054af8f33a
PrajwalNeupaneDgaf <157794663+PrajwalNeupaneDgaf@users.noreply.github.com>
2025-08-18 00:16:29 +0545

    Update LoadingComponent.js

--- app/Components/LoadingComponent.js
+++ app/Components/LoadingComponent.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import { BiLoader } from 'react-icons/bi'
 
 const LoadingComponent = () => {
   return (
-    <div className='h-[100dvh] w-[100dvw] flex justify-center items-center'>
-        <div>
-            
-        </div>
+    <div className='h-screen w-full flex justify-center items-center'>
+      <BiLoader className='animate-spin' size={'3rem'} />
     </div>
   )
 }
